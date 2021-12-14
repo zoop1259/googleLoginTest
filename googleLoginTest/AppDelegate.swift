50eb26a8375bf989991835424feecab4f1eceb21
zoop1259 <kdm432191@gmail.com>
2021-12-15 00:37:05 +0900

    일단 로그인구성

--- googleLoginTest/AppDelegate.swift
+++ googleLoginTest/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,9 +15,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+            if error != nil || user == nil {
+              // Show the app's signed-out state.
+            } else {
+              // Show the app's signed-in state.
+            }
+          }
+        
         return true
     }
 
+    func application(
+      _ app: UIApplication,
+      open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+        
+        
+      var handled: Bool
+
+      handled = GIDSignIn.sharedInstance.handle(url)
+      if handled {
+        return true
+      }
+
+      // Handle other custom URL types.
+
+      // If not handled by this app, return false.
+      return false
+    }
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
--- googleLoginTest/ViewController.swift
+++ googleLoginTest/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 class ViewController: UIViewController {
 
+    let signInConfig = GIDConfiguration.init(clientID: "257551777234-fm5l767d3t8a41k84km2bkc9op0t9816.apps.googleusercontent.com")
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func GoogleBtn(_ sender: Any) {
+        GIDSignIn.sharedInstance.signIn(with: signInConfig, presenting: self) { user, error in
+            guard error == nil else { return }
+  
+            //스타일?
+//            GIDSignInButtonColorScheme.dark
+//            GIDSignInButtonStyle.iconOnly
+            
+            guard let user = user else { return }
+
+                let emailAddress = user.profile?.email
+        }
+    
+    }
 
 }
 
